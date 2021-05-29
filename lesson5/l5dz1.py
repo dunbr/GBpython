494f02b570d15d2466e0f490f3b0ad09ad8a6a23
dunbr <dunb.g@nail.ru>
2021-05-29 23:46:12 +0300

    все задания к 5 уроку

---
+++ lesson5/l5dz1.py
@@ -0,0 +1,14 @@
+def add_nums(n):
+    for i in range(1, n + 1, 2):
+            yield print(i)
+add_to_15 = add_nums(15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
+next(add_to_15)
---
+++ lesson5/l5dz2(v2).py
@@ -0,0 +1,4 @@
+x = (i for i in range(1, 15 + 1, 2))
+
+for i in x:
+    print(i)
